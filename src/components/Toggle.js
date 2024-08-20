79c836828aeefa92a31187c4f68a362a0214e623
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-08-20 14:18:42 -0700

    SQ: This conditional operation returns the same value whether the condition is "true" or "false"

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -8,7 +8,7 @@ export default function Toggle({enabled, setEnabled}) {
       <Switch
         checked={enabled}
         onChange={setEnabled} data-testid="toggle"
-        className={`${enabled ? 'bg-dod-300' : 'bg-dod-300'}
+        className={`bg-dod-300'
           relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
       >
         <span className="sr-only">Use setting</span>
