1d0cd50eb3d77370607291887148869ccb770867
schittlur <78377530+schittlur@users.noreply.github.com>
2024-12-06 16:27:41 -0500

    user management screens

--- src/components/GraySecondaryButton.js
+++ src/components/GraySecondaryButton.js
@@ -4,7 +4,7 @@ export default function GraySecondaryButton({ buttonLabel, handleClick, icon }) 
   return (
     <button
         id={'view-course-button-'}
-        className='flex justify-center items-center gap-2 dod-500 w-84 rounded-lg hover:shadow-md text-purple bg-gray-100 hover:bg-purple hover:text-white px-2 p-1.5 py-2 transform transition-all duration-150 ease-in-out border-dod-500 border-2 focus:ring-2 ring-dod-500 outline-none'
+        className='px-4 flex justify-center items-center gap-2 dod-500 w-84 rounded-lg hover:shadow-md text-purple bg-gray-100 hover:bg-purple hover:text-white px-2 p-1.5 py-2 transform transition-all duration-150 ease-in-out border-dod-500 border-2 focus:ring-2 ring-dod-500 outline-none'
         title={buttonLabel}
         onClick={handleClick}
         >
