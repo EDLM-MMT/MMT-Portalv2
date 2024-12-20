eed12e9fe63eba5f0b733992e6f3cd5b0561ed27
Luis Reyes Madrid <lreyesmadrid@deloitte.com>
2024-12-20 14:00:12 -0500

    Implementation of announcements on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,17 @@ import DefaultLayout from '@/components/layouts/DefaultLayout';
 import SecondaryButton from '@/components/SecondaryButton';
 import GraySecondaryButton from '@/components/GraySecondaryButton';
 import MyUpdatesSections from '@/components/MyUpdatesSections';
+import Announcements from '@/components/Announcements';
 
 export default function Home() {
   const router = useRouter();
   const { user } = useAuth();
+
+  const data = [
+    {date:"December 20, 2024", time: "12:44 PM EST", announcement: "MMT is temporarily down. Service will be restored as soon as possible. Current estimate is by the end of the day.", type: "persist"},
+    {date:"January 30, 2025", time: "12:44 PM EST", announcement: "ARMY, NAVY, MARINE CORPS, SPACE FORCE: All veterans, active duty, National Guard Personnel should use the Joint Services Transcript (JST). AIR FORCE: All veterans, active duty, National Guard Personnel should use the re-designed Modernized Military Transcript (MMT).", type: "dismiss"},
+    
+];
   
   return (
     <DefaultLayout>
@@ -21,6 +28,8 @@ export default function Home() {
         <title>MMT</title>
         <link rel="icon" href="/logo.png" />
       </Head>
+
+      <Announcements content={data}/>
 
       <div className='flex flex-col mt-8'>
         <div className='py-4 text-xl font-bold'>Welcome, E-5, Julio Gomez! </div>
